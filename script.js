9cc37d357bc97a637d69906a661e32b516159bc5
Pedro Lucas <82179817+PedroLucasGarcia@users.noreply.github.com>
2025-02-14 21:28:24 +0000

    Update

--- script.js
+++ script.js
@@ -15,14 +15,6 @@ hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
 
-/*function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}*/
-
 //------------------------------------------------------
 
 const carousel = document.getElementById("carousel");
