49cbe93cf146f2237aca92552027316addb7edf6
Pedro Lucas <82179817+PedroLucasGarcia@users.noreply.github.com>
2025-02-15 21:45:22 +0000

    Update media queries

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-window.addEventListener("scroll", function () {
+/*window.addEventListener("scroll", function () {
     var header = document.getElementById("header");
     if (window.scrollY > 50) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
     }
-});
+});*/
+
+//------------------------------------------------------
 
 // Menu Hamburger
 const hamburger = document.querySelector(".hamburger");
